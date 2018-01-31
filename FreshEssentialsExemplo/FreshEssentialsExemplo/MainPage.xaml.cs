2b48272f35377c731b585deb58da48bd7896c344
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-01-31 13:48:49 -0200

    Add project files.

---
+++ FreshEssentialsExemplo/FreshEssentialsExemplo/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FreshEssentialsExemplo
+{
+    public partial class MainPage : ContentPage
+    {
+        int _index;
+        public int SelectIndex
+        {
+            get
+            {
+                return _index;
+            }
+            set
+            {
+                _index = value;
+                OnPropertyChanged("SelectIndex");
+            }
+        }
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+    }
+}
